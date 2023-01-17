0eff35063bb07ced7cc01e0e5886ba7c32580ab0
Moon ijaz <moonijaz006@gmail.com>
2023-01-17 10:30:00 +0500

    Add files

--- Contact-manager/src/App.jsx
+++ Contact-manager/src/App.jsx
@@ -1,21 +1,15 @@
+
 import React from 'react'
-import { useState } from 'react'
-import Header from "./Components/Header"
-import AddContact from "./Components/AddContact"
-import ContactCard from "./Component/ContactCard"
-import AddContactList from "./Components/AddContactList"
+import Header from "./Components/Header";
+
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header/>
-      <AddContact/>
-      <ContactCard/>
-      <AddContactList/>
     </>
   )
 }
