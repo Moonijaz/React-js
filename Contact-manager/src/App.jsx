a9db949862a85039a4a72e2938065b596fa84390
Moon ijaz <moonijaz006@gmail.com>
2023-01-23 10:30:00 +0500

    Add header changes

--- Contact-manager/src/App.jsx
+++ Contact-manager/src/App.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import Header from "./Components/Header";
+import AddContact from './Components/AddContact';
 
 
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Header/>
+      <AddContact/>
     </>
   )
 }
