6d7bb3122d7c80e59e26c75682d3fa540b482a63
Moon ijaz <moonijaz006@gmail.com>
2023-02-23 10:30:00 +0500

    Add to contact

--- Contact-manager/src/Components/AddContact.jsx
+++ Contact-manager/src/Components/AddContact.jsx
@@ -1,39 +1,4 @@
-<<<<<<< HEAD
-jsx
-import React from "react";
 
-export default class AddContact extends React.Component {
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const name = event.target.Name.value.trim();
-    const email = event.target.Email.value.trim();
-
-    // Perform input validation here
-
-    // Proceed with adding the contact
-  };
-jsx
-render() {
-  return (
-    <div className="ui-main">
-      <h2>Add Contact</h2>
-      <form className="ui form" onSubmit={this.handleSubmit}>
-        <div className="field">
-          <label>Name</label>
-          <input type="text" name="Name" placeholder="Name" required />
-          <br></br>
-          <label>Email</label>
-          <input type="email" name="Email" placeholder="Email" required />
-          <button className="ui button blue" type="submit">
-            Add
-          </button>
-        </div>
-      </form>
-    </div>
-  );
-}
-    }
-=======
 import React from "react";
 
 export default class AddContact extends React.Component{
@@ -52,4 +17,4 @@ export default class AddContact extends React.Component{
         )
     }
 }
->>>>>>> AddContact
+
