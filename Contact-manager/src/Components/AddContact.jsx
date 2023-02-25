f1ae557325b9b9c4379a856ba9a38bf574207236
Moon ijaz <moonijaz006@gmail.com>
2023-02-25 12:00:00 +0500

    Add file ti jsx

--- Contact-manager/src/Components/AddContact.jsx
+++ Contact-manager/src/Components/AddContact.jsx
@@ -1,11 +1,11 @@
 
 import React from "react";
-
+import './AddContact.css'
 export default class AddContact extends React.Component{
     render(){
         return (
             <div className="ui-main">
-                <h2>Add Contact</h2>
+                <h2 className="h1">Add Contact</h2>
                 <form className="ui form">
                     <div className="field">
                         <label>Name</label> <input type="text"  name="Name" placeholder="Name" /> <br></br>
