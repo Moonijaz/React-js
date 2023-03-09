3bc65c8f7262617c2681f6d4869fc0a659db175f
Moon ijaz <moonijaz006@gmail.com>
2023-03-09 08:00:00 +0500

    Add header component

---
+++ Todo-list/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+export default function Header() {
+  return (
+    <div className='head'>
+            <h2>Contact Manager</h2>
+            <hr class="shadow"/>
+    </div>
+  )
+}
