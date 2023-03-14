11cee8fc7e0316fca1da95dcb975ec457f262c58
Moon ijaz <moonijaz006@gmail.com>
2023-03-14 08:00:00 +0500

    Add components in app.jsx

--- Todo-list/src/App.jsx
+++ Todo-list/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './Components/Header';
-
+import Addlist from './Components/Addlist';
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Addlist/>
+      <Todo-list/>
     </>
   )
 }
