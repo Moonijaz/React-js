7b233e871ed7963cca866e1f3e542825d9495677
Moon ijaz <moonijaz006@gmail.com>
2023-03-23 08:00:00 +0500

    Add to Addlist

--- Todo-list/src/Components/Addlist.jsx
+++ Todo-list/src/Components/Addlist.jsx
@@ -1,12 +1,27 @@
-
-
-import React from 'react'
+import React, { useState } from 'react';
 import './Header.css'
-export default function Addlist() {
+export default function Addlist({onAddlist}) {
+        const [name, setName] = useState('');
+      
+        const handleSubmit = (e) => {
+          e.preventDefault();
+      
+          if (name) {
+            const newlist = {
+              name: name,
+            };
+      
+            onAddlist(newlist);
+      
+            // Clear input fields
+            setName('');
+          }
+        };
+
   return (
     <div className='head'>
             <h3>Add list</h3>
-          <input type="text" value={item} onChange={(e) => setName(e.target.value)} placeholder="Add your task/items" /> <br /><br />
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Add your task/items" /> 
           <button className="btn btn-primary" type="submit">Add</button>
 
     </div>
