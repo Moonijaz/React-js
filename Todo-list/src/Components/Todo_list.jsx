e82915989c51b3ffc5deead8ef6d1c0c59a2e9e1
Moon ijaz <moonijaz006@gmail.com>
2023-04-01 01:00:00 +0500

    Add ondelete icon in list function

--- Todo-list/src/Components/Todo_list.jsx
+++ Todo-list/src/Components/Todo_list.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 
-function Lists({ itemtoadd }) {
+function Lists({ itemtoadd, onDeleteItem }) {
   return (
     <div>
       <h2>Items in list</h2>
       <ol>
-        {itemtoadd && Array.isArray(itemtoadd) ? (
-          itemtoadd.map((list, index) => (
-            <li key={index}>
-             {list.name}
-            </li>
-          ))
-        ) : (
-          <li>No items to display</li>
-        )}
+        {itemtoadd.map((list, index) => (
+          <li key={list.id}>
+            Name: {list.name}
+            <button onClick={() => onDeleteItem(list.id)}>Delete</button>
+          </li>
+        ))}
       </ol>
     </div>
   );
 }
+
 
 export default Lists;
