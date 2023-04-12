daaac1c9d2bf8c14f13ded236db2e569308c7f76
Moon ijaz <moonijaz006@gmail.com>
2023-04-13 01:00:00 +0500

    Add div to main

--- Todo-list/src/Components/Todo_list.jsx
+++ Todo-list/src/Components/Todo_list.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './List.css'
 function Lists({ itemtoadd, onDeleteItem }) {
   return (
-    <div>
+    <div className='list-main'>
       <h2>Items in list</h2>
       <ol>
         {itemtoadd.map((list, index) => (
