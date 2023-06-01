0ae8fa37529f460510188e2a751a923408b7050b
Moon ijaz <moonijaz006@gmail.com>
2023-06-02 02:44:00 +0500

    Add Navbar.jsx file

--- responsive-navbar/src/App.jsx
+++ responsive-navbar/src/App.jsx
@@ -1,4 +1,4 @@
-
+import Navbar from './Components/Navbar'
 import './App.css'
 
 function App() {
@@ -6,7 +6,8 @@ function App() {
 
   return (
     <>
-     
+     <p>bxjsjhj</p>
+     <Navbar/>
     </>
   )
 }
