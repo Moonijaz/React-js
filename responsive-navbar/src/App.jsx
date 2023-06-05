2088ff026771cd85f222d120bf0aecc3b0f9368e
Moon ijaz <moonijaz006@gmail.com>
2023-06-05 05:34:00 +0500

    Add comments to navbar

--- responsive-navbar/src/App.jsx
+++ responsive-navbar/src/App.jsx
@@ -1,29 +1,22 @@
-import React from 'react';
-//import { BrowserRouter } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
-import { Router } from 'react-router-dom';
-//import { Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './components/HomePage';
-import CoursesPage from './Components/CoursesPage';
-import DegreesPage from './components/DegreesPage';
-import BusinessPage from './components/BusinessPage';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import CoursesPage from "./Components/CoursesPage";
+import DegreesPage from "./components/DegreesPage";
+import BusinessPage from "./components/BusinessPage";
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>  {/* Setting up BrowserRouter to enable routing in the app*/}
+      <Navbar />
       <Routes>
-      <div>
-        <Navbar />
-
-          <Route path="/" exact component={HomePage} />
-          <Route path="/courses" component={CoursesPage} />
-          <Route path="/degrees" component={DegreesPage} />
-          <Route path="/business" component={BusinessPage} />
-
-      </div>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/BusinessPage" element={<BusinessPage />}></Route>
+        <Route path="/CoursesPage" element={<CoursesPage />}></Route>
+        <Route path="/DegreesPage" element={<DegreesPage />}></Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
--- responsive-navbar/src/Components/Navbar.jsx
+++ responsive-navbar/src/Components/Navbar.jsx
@@ -20,28 +20,12 @@ export default function Navbar() {
           <div className="navbar-toggle" onClick={handleIcon}>
             <span className="navbar-toggle-icon"></span>
           </div>
-          <div className={`navbar-menu ${isTogled ? '' : 'active'}`}>
+          <div className={`navbar-menu ${isTogled ? '' : 'active'}`}> {/*we have used this instead of if else */}
             <ul className="navbar-items">
-              <li className="navbar-item">
-                <Link to="/" className="navbar-link">
-                  Home
-                </Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/coursesPage" className="navbar-link">
-                  Courses
-                </Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/degreesPage" className="navbar-link">
-                  Degrees
-                </Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/businessPage" className="navbar-link">
-                  For Business
-                </Link>
-              </li>
+              <li className="navbar-item"> <Link to="/" className="navbar-link"> Home </Link> </li>
+              <li className="navbar-item"> <Link to="/coursesPage" className="navbar-link"> Courses </Link> </li>
+              <li className="navbar-item"> <Link to="/degreesPage" className="navbar-link"> Degrees </Link> </li>
+              <li className="navbar-item"> <Link to="/businessPage" className="navbar-link"> For Business </Link> </li>
             </ul>
           </div>
         </div>
