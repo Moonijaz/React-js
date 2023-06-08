c9097bc3346d605abdb5507261ff94fb896dff21
Moon ijaz <moonijaz006@gmail.com>
2023-06-08 05:34:00 +0500

    Add props to navbar

--- Space-website/src/App.jsx
+++ Space-website/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/navbar/navbar";
+import HomePage from "./Components/HomePage/HomePage";
+import CoursesPage from "./Components/CoursesPage/CoursesPage";
+import DegreesPage from "./Components/DegreesPage/DegreesPage";
+import BusinessPage from "./Components/BusinessPage/BusinessPage";
 
-function App() {
-
-
+const App = () => {
   return (
-    <>
-      <h1>hi</h1>
-    </>
-  )
-}
+    <BrowserRouter>  {/* Setting up BrowserRouter to enable routing in the app*/}
+      <Navbar home= "Home" Courses= "Courses" Degrees= "Degrees" For_Business="For Business"/> {/* Setting props values here that will be displayed in navbar*/}
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/BusinessPage" element={<BusinessPage />}></Route>
+        <Route path="/CoursesPage" element={<CoursesPage />}></Route>
+        <Route path="/DegreesPage" element={<DegreesPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
--- Space-website/src/Components/navbar/navbar.jsx
+++ Space-website/src/Components/navbar/navbar.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import "./Navbar.css"
 
-const navbar = () => {
+export default function Navbar(props) {
+  const [isTogled, setTogle] = useState(true);
+
+  const handleIcon = () => {
+    setTogle(!isTogled);
+  };
+
   return (
-    <div>navbar</div>
-  )
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            Coursera
+          </Link>
+          <div className="navbar-toggle" onClick={handleIcon}>
+            <span className="navbar-toggle-icon"></span>
+          </div>
+          <div className={`navbar-menu ${isTogled ? '' : 'active'}`}> {/*we have used this instead of if else */}
+            <ul className="navbar-items">
+              <li className="navbar-item"> <Link to="/" className="navbar-link"> {props.home} </Link> </li>
+              <li className="navbar-item"> <Link to="/coursesPage" className="navbar-link"> {props.Courses} </Link> </li>
+              <li className="navbar-item"> <Link to="/degreesPage" className="navbar-link"> {props.Degrees} </Link> </li>
+              <li className="navbar-item"> <Link to="/businessPage" className="navbar-link"> {props.For_Business} </Link> </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default navbar
+Navbar.prototype = {
+    home : PropTypes.string,
+    Courses : PropTypes.string,
+    Degrees : PropTypes.string,
+    For_Business : PropTypes.string,
+}
