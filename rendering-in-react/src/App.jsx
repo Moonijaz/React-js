20fd9ca261dd7b3ee7b81069e58db21fb88349d4
Moon ijaz <moonijaz006@gmail.com>
2023-06-22 15:58:09 +0500

    Add 3 methods for rendering using usestate hook : 1. using if-else 2. using only if statement 3. rendering items from a list

--- rendering-in-react/src/App.jsx
+++ rendering-in-react/src/App.jsx
@@ -1,25 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [showbtn, setshowbtn] = useState(false) // when true will show button 
+  const [todo, settodo] = useState([
+    {
+      title: "Hey",
+      desc : "i am a todo"
+    },
+    {
+      title: "Hey Another todo",
+      desc : "i am another todo"
+    },
+    {
+      title: "Hey i am third todo",
+      desc : "i am a third todo"
+    },
+  ])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Vite + React</h1>
+
+        {/* button will only appear when usestate is true otherwise  it will show none. This is called conditional rendering */}
+        {/* if else rendering */}
+      {showbtn?<button>showbtn is true</button> : <button>showbtn is false</button>}  
+
+        {/* if rendering */}
+      {/* {showbtn && <button>i will be shown only second button in clicked</button>} */}
+
+      {/* rendering using list */}
+      {todo.map(todo =>{
+        return <div key={todo.title} > 
+        <div>{todo.title}</div>
+        <div>{todo.desc}</div>
+        </div>
+      })}
+      
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setshowbtn(!showbtn)} >
+          Toggle showbtn
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
