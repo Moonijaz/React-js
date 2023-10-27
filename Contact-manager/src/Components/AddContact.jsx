dbc23f756e194e2e44e1c8fd508050f5e10d7912
Moon ijaz <moonijaz006@gmail.com>
2023-10-27 18:23:25 +0500

    Add css to jsx

--- Contact-manager/src/Components/AddContact.jsx
+++ Contact-manager/src/Components/AddContact.jsx
@@ -8,9 +8,9 @@ export default class AddContact extends React.Component{
                 <h2 className="h1">Add Contact</h2>
                 <form className="ui form">
                     <div className="field">
-                        <label>Name</label><br /> <input type="text"  name="Name" placeholder="Name" /> <br></br>
-                        <label>Email</label> <br /><input type="text"  name="Email" placeholder="Email" /> <br></br>
-                        <button className="ui button blue">Add</button>
+                        <label className="lable">Name</label><input type="text"  name="Name" placeholder="Name" /> <br></br><br></br>
+                        <label className="lable">Email</label> <input type="text"  name="Email" placeholder="Email" /> <br></br><br></br>
+                        <button className="ui-button-blue">Add</button>
                     </div>
                 </form>
             </div>
