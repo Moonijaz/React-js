842b1599d9d29fd0f91bc95592b23644c11346a2
Moon ijaz <moonijaz006@gmail.com>
2023-10-27 18:47:53 +0500

    Add css

--- Contact-manager/src/Components/AddContact.jsx
+++ Contact-manager/src/Components/AddContact.jsx
@@ -8,8 +8,8 @@ export default class AddContact extends React.Component{
                 <h2 className="h1">Add Contact</h2>
                 <form className="ui form">
                     <div className="field">
-                        <label className="lable">Name</label><input type="text"  name="Name" placeholder="Name" /> <br></br><br></br>
-                        <label className="lable">Email</label> <input type="text"  name="Email" placeholder="Email" /> <br></br><br></br>
+                        <label >Name</label><input type="text"  name="Name" placeholder="Name" /> <br></br><br></br>
+                        <label >Email</label> <input type="text"  name="Email" placeholder="Email" /> <br></br><br></br>
                         <button class="btn btn-primary" type="submit">Add</button>
                     </div>
                 </form>
