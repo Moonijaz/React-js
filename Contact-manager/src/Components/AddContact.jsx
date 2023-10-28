63bb83f695b868c5c4e46a8f97fe9a3e040660f2
Moon ijaz <moonijaz006@gmail.com>
2023-10-28 14:55:46 +0500

    Add changes into file

--- Contact-manager/src/Components/AddContact.jsx
+++ Contact-manager/src/Components/AddContact.jsx
@@ -1,6 +1,6 @@
-
 import React from "react";
 import './AddContact.css'
+
 export default class AddContact extends React.Component{
     render(){
         return (
@@ -10,7 +10,7 @@ export default class AddContact extends React.Component{
                     <div className="field">
                         <label >Name</label><input type="text"  name="Name" placeholder="Name" /> <br></br><br></br>
                         <label >Email</label> <input type="text"  name="Email" placeholder="Email" /> <br></br><br></br>
-                        <button class="btn btn-primary" type="submit">Add</button>
+                        <button className="btn btn-primary" type="submit">Add</button>
                     </div>
                 </form>
             </div>
