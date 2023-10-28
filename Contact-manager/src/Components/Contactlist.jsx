30d4891d6adceb770fdbbdceb728e0c9948d6219
Moon ijaz <moonijaz006@gmail.com>
2023-10-28 17:27:30 +0500

    Add a contact list

---
+++ Contact-manager/src/Components/Contactlist.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contactlist = ( ) => {
+    return (
+    <div className="contactlist">
+         Contact List
+         </div>
+    );
+};
+
+export default Contactlist;
