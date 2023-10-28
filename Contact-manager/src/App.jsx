6491ef88d46ce977b60be7d2d344a64304ec61e8
Moon ijaz <moonijaz006@gmail.com>
2023-10-28 18:54:17 +0500

    Add contact list file and Addcontact file in app.js

--- Contact-manager/src/App.jsx
+++ Contact-manager/src/App.jsx
@@ -1,22 +1,27 @@
-
-import React from 'react'
+import React, { useState } from 'react';
 import Header from "./Components/Header";
-//import AddContact from './Components/AddContact';
+import AddContact from './Components/AddContact';
 import Contactlist from './Components/Contactlist';
 
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const handleAddContact = (newContact) => {
+    setContacts([...contacts, newContact]);
+  };
+
 
   return (
     <>
     <div className='container'> 
       <Header/>
-     {/* <AddContact/> */}
-      <Contactlist/>
+      <AddContact onAddContact={handleAddContact} />
+      <Contactlist contacts={contacts} />
     </div>
 
     </>
   )
-}
+  }
 
-export default App
+export default App;
