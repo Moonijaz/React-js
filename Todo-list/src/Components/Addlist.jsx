a8e290aa52ec8331ad802f431d3b838113a38c57
Moon ijaz <moonijaz006@gmail.com>
2023-10-30 16:38:25 +0500

    Add value as item

--- Todo-list/src/Components/Addlist.jsx
+++ Todo-list/src/Components/Addlist.jsx
@@ -6,7 +6,7 @@ export default function Addlist() {
   return (
     <div className='head'>
             <h3>Add list</h3>
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Add your task/items" /> <br /><br />
+          <input type="text" value={item} onChange={(e) => setName(e.target.value)} placeholder="Add your task/items" /> <br /><br />
           <button className="btn btn-primary" type="submit">Add</button>
 
     </div>
