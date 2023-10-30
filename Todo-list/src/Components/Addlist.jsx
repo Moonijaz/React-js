9d63408e23155bc1eb411e73b021b12432daa999
Moon ijaz <moonijaz006@gmail.com>
2023-10-30 16:36:13 +0500

    Add list

---
+++ Todo-list/src/Components/Addlist.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+import './Header.css'
+export default function Addlist() {
+  return (
+    <div className='head'>
+            <h3>Add list</h3>
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Add your task/items" /> <br /><br />
+          <button className="btn btn-primary" type="submit">Add</button>
+
+    </div>
+  )
+}
