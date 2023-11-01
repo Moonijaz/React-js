e22c8e154999a2db6953dc68412431b5643dc7f7
Moon ijaz <moonijaz006@gmail.com>
2023-11-01 19:53:59 +0500

    Add todo_list

--- Todo-list/src/App.jsx
+++ Todo-list/src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css'
 import Header from './Components/Header';
 import Addlist from './Components/Addlist';
-import todolist from './Components/Todo-list';
+import Lists from './Components/Todo_list';
+import React, { useState } from 'react'; // Import React and useState
+
 function App() {
+  const [itemList, setItemList] = useState([]); // Define a state to hold the item list
+
+  const handleAddlist = (newlist) => {
+    // Function to add a new list item
+    setItemList([...itemList, newlist]);
+  };
 
   return (
     <>
-      <Header/>
-      <Addlist/>
-      <todolist/>
+      <Header />
+      <Addlist onAddlist={handleAddlist} /> {/* Pass the onAddlist prop */}
+      <Lists itemtoadd={itemList} /> {/* Pass the itemtoadd prop */}
     </>
   )
 }
 
-export default App
+export default App;
