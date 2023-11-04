7e3b6b763bc66502287f7bbef208a478a97155c7
Moon ijaz <moonijaz006@gmail.com>
2023-11-04 19:47:21 +0500

    Add buttons

--- RPS-app/src/Components/Game.jsx
+++ RPS-app/src/Components/Game.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import './Game.css'
 
 export default function Game() {
+
+   
+
   return (
     <div className='main'>
-        Rock, Paper , Scissor Game
+        <div className='Header'>  Rock, Paper , Scissor Game </div>
+        <button>Rock</button>
+        <button>Paper</button>
+        <button>Scissor</button>
+
     </div>
   )
 }
