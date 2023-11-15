48cbc160cfbd63052a80aa6a20749fbe7d0dadd6
Moon ijaz <moonijaz006@gmail.com>
2023-11-15 12:54:48 +0500

    Add css to navbar

--- responsive-navbar/src/Components/Navbar.jsx
+++ responsive-navbar/src/Components/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import './Navbar.css'
 export default function Navbar() {
   return (
    <>
-   <p>xnxjhxjk</p>
+ <nav class="navbar">
+        <div class="navbar-container">
+          <a class="navbar-logo" href="#">Coursera</a>
+          <div class="navbar-toggle">
+            <span class="navbar-toggle-icon"></span>
+          </div>
+          <div class="navbar-menu">
+            <ul class="navbar-items">
+              <li class="navbar-item"><a class="navbar-link" href="#">Home</a></li> {/*seperate css for item bcz we want to give space between each item */}
+              <li class="navbar-item"><a class="navbar-link" href="#">Courses</a></li>
+              <li class="navbar-item"><a class="navbar-link" href="#">Degrees</a></li>
+              <li class="navbar-item"><a class="navbar-link" href="#">For Business</a></li>
+            
+            </ul>
+          </div>
+        </div>
+      </nav>
    </>
   )
 }
