2436bfe6351656fee8150e05362e28e7edff63d0
Moon ijaz <moonijaz006@gmail.com>
2023-11-16 15:07:42 +0500

    Add usestate to togle icon

--- responsive-navbar/src/Components/Navbar.jsx
+++ responsive-navbar/src/Components/Navbar.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 import './Navbar.css'
 export default function Navbar() {
+ const [isToogled , setToogle] = useState(false); {/*initially we want to hise our tems thats why we have used here false */}
+
+ const handleIcon = () => {
+   setToogle(!isToogled);
+ }
+
   return (
    <>
- <nav class="navbar">
-        <div class="navbar-container">
-          <a class="navbar-logo" href="#">Coursera</a>
-          <div class="navbar-toggle">
-            <span class="navbar-toggle-icon"></span>
+ <nav className="navbar">
+        <div className="navbar-container">
+          <a className="navbar-logo" href="#">Coursera</a>
+          <div className="navbar-toggle" onClick={handleIcon}>
+            <span className="navbar-toggle-icon" ></span>
           </div>
-          <div class="navbar-menu">
-            <ul class="navbar-items">
-              <li class="navbar-item"><a class="navbar-link" href="#">Home</a></li> {/*seperate css for item bcz we want to give space between each item */}
-              <li class="navbar-item"><a class="navbar-link" href="#">Courses</a></li>
-              <li class="navbar-item"><a class="navbar-link" href="#">Degrees</a></li>
-              <li class="navbar-item"><a class="navbar-link" href="#">For Business</a></li>
+          <div className= {`navbar-menu ${isToogled ? '' : 'active'}`}> {/*it is better to use template string instead of if else */}
+            <ul className="navbar-items">
+              <li className="navbar-item"><a className="navbar-link" href="#">Home</a></li> {/*seperate css for item bcz we want to give space between each item */}
+              <li className="navbar-item"><a className="navbar-link" href="#">Courses</a></li>
+              <li className="navbar-item"><a className="navbar-link" href="#">Degrees</a></li>
+              <li className="navbar-item"><a className="navbar-link" href="#">For Business</a></li>
             
             </ul>
           </div>
